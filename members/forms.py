b9a0e18f6aeebc1d23f8af557b2fc73b0330d567
meron <merronmuche@gmail.com>
2022-11-05 06:21:21 -0300

    inherit from model form

--- members/forms.py
+++ members/forms.py
@@ -1,10 +1,15 @@
 
 
 from django import forms
+from members.models import Members
+class MembersForm(forms.ModelForm):
 
-class MembersForm(forms.Form):
+    # firstname = forms.CharField(max_length=255)
+    # lastname = forms.CharField(max_length=255)
+    # age = forms.IntegerField()
 
-    firstname = forms.CharField(max_length=255)
-    lastname = forms.CharField(max_length=255)
-    age = forms.IntegerField()
+    class Meta:
+        model = Members
+        # fields = '__all__'
+        fields = ['firstname', 'lastname']
 
