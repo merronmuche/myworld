e620e8d33bf237979b9f35f0eb68c3effae61f4e
meron <merronmuche@gmail.com>
2022-11-05 05:11:00 -0300

    list table values using html

--- members/admin.py
+++ members/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from members.models import Members
+
+class MembersAdmin(admin.ModelAdmin):
+
+    list_display = ['firstname', 'lastname', 'age']
+
+admin.site.register(Members, MembersAdmin)
--- members/views.py
+++ members/views.py
@@ -1,10 +1,18 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.template import loader
 from members.models import Members
 
 def index(request):
-  template = loader.get_template('myfirst.html')
-  return HttpResponse(template.render())
+  
+  members = Members.objects.all()
+
+  context = {
+    'members':members
+  }
+  return render(request, 'members/myfirst.html', context)
+
+
 def new_mod(request):
     Members.objects.create(firstname= 'markos',lastname='amsalu',age=30)
     return HttpResponse('Created successuulyly')
